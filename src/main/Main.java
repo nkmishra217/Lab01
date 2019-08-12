1ace56a83915d58233da6155e3f7507bbba23d25
nkmishra217 <deeksha.mishra_cs18@gla.ac.in>
2019-08-12 15:57:47 +0530

    Main.java: add value to a defined variable

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,13 @@
  * */
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
     }
 
 
